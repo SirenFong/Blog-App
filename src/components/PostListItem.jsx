e3cb81e10cbd5b264a029f3a41699812ba49fb38
Hakate <audition250601@gmail.com>
2025-03-17 23:04:15 +0700

    Update PostListItem component layout and styling

--- src/components/PostListItem.jsx
+++ src/components/PostListItem.jsx
@@ -5,20 +5,23 @@ const PostListItem = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-8">
       {/*image*/}
-      <div className="md:hidden xl:block">
-        <Image src="postImg.jpeg" className="rounded-2xl object-cover" />
+      <div className="w-full xl:w-3/4">
+        <Image 
+          src="postImg.jpeg" 
+          className="w-full h-[200px] xl:h-[400px] rounded-2xl object-cover" 
+        />
       </div>
 
       {/*details*/}
-      <div className="">
-        <Link to="/test" className="text-4xl font-semibold">
+      <div className="w-full xl:w-1/4">
+        <Link to="/test" className="text-xl xl:text-2xl font-semibold hover:text-green-800">
           Đây là nơi bắt đầu
         </Link>
-        <div className="">
+        <div className="mt-2 flex flex-col xl:flex-row xl:flex-wrap items-start xl:items-center gap-2 text-gray-600">
           <span>Tạo bởi</span>
-          <Link>Bé Su</Link>
+          <Link className="text-green-800 hover:underline">Bé Su</Link>
           <span>Trong mục</span>
-          <Link>Cà phê</Link>
+          <Link className="text-green-800 hover:underline">Cà phê</Link>
         </div>
       </div>
     </div>
